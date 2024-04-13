9808ce5bcf2913dc342c199d45f67c0eb863cb90
Savin Mikhail <mikhail.d.savin@gmail.com>
2024-04-13 20:23:53 +0700

    feat: enc stream decorator

--- src/EncryptingStream.php
+++ src/EncryptingStream.php
@@ -4,8 +4,80 @@ namespace src;
 
 use GuzzleHttp\Psr7\StreamDecoratorTrait;
 use Psr\Http\Message\StreamInterface;
+use src\Enums\MediaTypeEnum;
+use src\Exceptions\CryptException;
 
-class EncryptingStream implements StreamInterface
+class EncryptingStream extends Encryption implements StreamInterface
 {
     use StreamDecoratorTrait;
+    protected const BLOCK_SIZE = 16; // AES block size is 16 bytes (128 bits)
+    protected const /*string*/ CIPHER_ALGORITHM = 'aes-256-cbc';
+
+    public function __construct(protected StreamInterface $stream)
+    {
+    }
+
+    public function isWritable(): false
+    {
+        return false;
+    }
+
+    public function read($length): string
+    {
+           return $this->encryptBlock($length);
+    }
+
+    private function encryptBlock(int $length): string
+    {
+        if ($this->stream->eof()) {
+            return '';
+        }
+        $this->mediaType =  MediaTypeEnum::DOCUMENT;
+
+        $count = ceil($length/self::BLOCK_SIZE);
+        $encryptedData = '';
+        $mediaKey = $this->generateMediaKey();
+        //2. Expand it
+        $mediaKeyExpanded = $this->getExpandedMediaKey($mediaKey);
+
+        //3. Split `mediaKeyExpanded`
+        [$iv, $cipherKey, $macKey] = $this->splitExpandedKey($mediaKeyExpanded);
+        $this->macKey = $macKey;
+        $this->iv = $iv;
+
+        do {
+            $chunk = $this->stream->read(self::BLOCK_SIZE);
+            // Check if this is the last chunk
+            $isLastChunk = $this->stream->eof();
+
+            $options = OPENSSL_RAW_DATA;
+            // Apply padding only if it's not the last chunk
+            if (! $isLastChunk) {
+                $options |= OPENSSL_ZERO_PADDING;
+            }
+
+            // Encrypt the chunk of data
+            $encryptedChunk = openssl_encrypt(
+                data: $chunk,
+                cipher_algo:  self::CIPHER_ALGORITHM,
+                passphrase: $cipherKey,
+                options: $options,
+                iv: $this->getCurrentIv(),
+            );
+
+            if ($encryptedChunk === false) {
+                throw new CryptException('Failed to encrypt data: '.openssl_error_string());
+            }
+
+            // Append the encrypted chunk to the encrypted data
+            $encryptedData .= $encryptedChunk;
+            $this->updateIv($encryptedChunk);
+            $count--;
+        } while ($count > 0 || !$this->stream->eof());
+
+
+
+        return $encryptedData;
+    }
+
 }
